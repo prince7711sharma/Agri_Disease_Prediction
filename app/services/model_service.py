cc914e7d6e1cdf76d4944f3d0146a178ef639fb7
Prince <169139950+prince7711sharma@users.noreply.github.com>
2026-03-27 15:35:01 +0530

    Update model_service.py

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -1,5 +1,5 @@
-#import tensorflow as tf
-import keras
+import tensorflow as tf
+#import keras
 import numpy as np
 import json
 import os
@@ -19,7 +19,7 @@ class ModelService:
 
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-        model_path = os.path.join(BASE_DIR, "../../model/plant_disease_model.keras")
+        model_path = os.path.join(BASE_DIR, "../../model/plant_disease_model.h5")
 
         # 🔥 ADD THESE LINES HERE
         print("MODEL PATH:", model_path)
@@ -31,8 +31,13 @@ class ModelService:
             print("❌ MODEL FILE NOT FOUND")
 
         # ✅ Load model
-        self.model = keras.models.load_model(model_path, compile=False)
+        self.model = tf.keras.models.load_model(
+              model_path,
+              compile=False
+        )
+         
 
+           
         print("✅ Model loaded")
         print(f"✅ Model loaded from: {model_path}")
 
